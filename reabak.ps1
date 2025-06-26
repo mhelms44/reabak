6fab6430d31917f1f1d6eabb54e73bcd3130c4d9
Michael Helms <mhelms92@gmail.com>
2025-06-26 08:40:05 -0400

    Add configurable path variables, improve source dir check

--- reabak.ps1
+++ reabak.ps1
@@ -5,7 +5,7 @@
 # Very crude logging for both the files + robocopy output.
 <#
     TODO
-    Change all1 the hard coded paths, etc. to variables
+    Change all the hard coded paths, etc. to variables
     Need to make robocopy only target the right file type
     Would be nice to have better logging in place
     Also need logic to auto rotate file list log after X size/number of days
@@ -13,23 +13,27 @@
     For now everything seems to work okay
 #>
 
+# Config
+$SourceDir = "F:\Reaper Project Backup Staging"
+$DestDir = "B:\Reaper Backup Archives"
+$LogFile = "$env:USERPROFILE\reabak.log"
+
 # Validate the path to the backup files exists and has files. If false
 # the script will stop here. 
-if (!(Test-Path -Path 'F:\Reaper Project Backup Staging\*'))
-{
+if (!(Test-Path -Path $SourceDir)) {
     Get-Date >> "C:\Users\Michael\reabak.txt"
-    "There is nothing to backup, exiting now" >> "C:\Users\Michael\reabak.txt"
+    "$SourceDir does not exist, nothing to do. Exit code 0" >> "C:\Users\Michael\reabak.txt"
+    exit 0
 }
 
 # All the good stuff happens here. If files are detected in the path robocopy 
 # Will move all files in the directory. 
-elseif (Test-Path -Path 'F:\Reaper Project Backup Staging\*')
-{
-# Commenting out this part for debug use. In future would be nice to set with a flag 
-#Get-Date >> "C:\Users\Michael\reabakERROR.txt"
-#"Starting Reaper project backup transfer. Here is a list of files up for transfer:" >> "C:\Users\Michael\reabakERROR.txt"
-#Get-ChildItem -Path "F:\Reaper Project Backup Staging\" -Filter *.rpp-bak -r | Format-List -Property ('Name') >> "C:\Users\Michael\reabakERROR.txt"
-robocopy 'F:\Reaper Project Backup Staging\ ' 'B:\Reaper Backup Archives\ ' /MOV /XX /NP /log:"C:\Users\Michael\reabak.txt"
+elseif (Test-Path -Path 'F:\Reaper Project Backup Staging\*') {
+    # Commenting out this part for debug use. In future would be nice to set with a flag 
+    #Get-Date >> "C:\Users\Michael\reabakERROR.txt"
+    #"Starting Reaper project backup transfer. Here is a list of files up for transfer:" >> "C:\Users\Michael\reabakERROR.txt"
+    #Get-ChildItem -Path "F:\Reaper Project Backup Staging\" -Filter *.rpp-bak -r | Format-List -Property ('Name') >> "C:\Users\Michael\reabakERROR.txt"
+    robocopy 'F:\Reaper Project Backup Staging\ ' 'B:\Reaper Backup Archives\ ' /MOV /XX /NP /log:"C:\Users\Michael\reabak.txt"
 }
 
 # Generic catch all. I can't imagine this would ever actually evaluate as true.
